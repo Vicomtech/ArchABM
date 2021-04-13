922d2ae8eecc19e337c5bd16b01d925cdd1ae288
imartinezl <inigomlap@gmail.com>
2021-04-13 15:46:22 +0200

    Engine: allow multiple simulations with same config

--- archABM/Engine.py
+++ archABM/Engine.py
@@ -1,41 +1,32 @@
 import simpy
+import logging
 
 from .Creator import Creator
 from .Database import Database
-import logging
-
 from .Actions import Actions
+from .Results import Results
 
 
 class Engine:
     def __init__(self, config):
         self.config = config
+        self.db = Database()
+        self.db.results = Results(self.config)
 
+    def setup(self):
         self.env = simpy.Environment()
-        self.db = Database(self.env, self.config)
+        self.db.next()
 
         god = Creator(self.env, self.config, self.db)
+        self.db.events = god.create_events()
+        self.db.places = god.create_places()
+        self.db.options = god.create_options()
+        self.db.actions = god.create_actions()
+        self.db.people = god.create_people()
 
-        events = god.create_events()
-        self.db.set_events(events)
-
-        places = god.create_places()
-        self.db.set_places(places)
-
-        # virus = god.create_virus()
-
-        people = god.create_people()
-        self.db.set_people(people)
-
-        options = god.create_options()
-        self.db.set_options(options)
-
-        actions = god.create_actions()
-        self.db.set_actions(actions)
-
-        # self.env.process(actions.create_random_event(600))
-
-    def run(self, until):
+    def run(self, until, num=1):
         # print("Simulation Started")
-        self.env.run(until)
+        for i in range(num):
+            self.setup()
+            self.env.run(until)
         # print("Simulation Finished")
