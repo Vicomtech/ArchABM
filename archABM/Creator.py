894a80dcd4394ba1367eecb7e4e35f2c754d172d
imartinezl <inigomlap@gmail.com>
2021-04-14 18:45:30 +0200

    Removed unnecessary imports + ordered methods

--- archABM/Creator.py
+++ archABM/Creator.py
@@ -1,16 +1,9 @@
 from .EventModel import EventModel
 from .Place import Place
-
-# from .PlaceParameters import PlaceParameters
 from .Person import Person
-
-# from .PersonParameters import PersonParameters
 from .Parameters import Parameters
 from .Actions import Actions
 from .Options import Options
-
-import numpy as np
-
 
 class Creator:
     def __init__(self, env, config, db):
@@ -21,18 +14,17 @@ class Creator:
         Person.reset()
         Place.reset()
 
+    def create_options(self):
+        p = self.config["options"]
+        params = Parameters(p)
+        options = Options(self.env, self.db, params)
+
+        return options
+
     def create_events(self):
         events = []
-        for e in self.config["Events"].replace({np.nan: None}).to_dict("records"):
+        for e in self.config["events"]:
             params = Parameters(e)
-            # event = EventModel(
-            #     e["activity"],
-            #     e["schedule"],
-            #     e["repeat_min"],
-            #     e["repeat_max"],
-            #     e["duration_min"],
-            #     e["duration_max"],
-            # ) # TODO: replace with Parameter? DONE
             event = EventModel(params)
             events.append(event)
 
@@ -40,45 +32,24 @@ class Creator:
 
     def create_places(self):
         places = []
-        df = self.config["Places"]
-        for p in self.config["Places"].replace({np.nan: None}).to_dict("records"):
+        for p in self.config["places"]:
             params = Parameters(p)
-            # params = PlaceParameters(
-            #     p["name"],
-            #     p["event"],
-            #     p["building"],
-            #     p["department"],
-            #     p["area"],
-            #     p["height"],
-            # )
-            # TODO: replace event string by event_model instance DONE
             place = Place(self.env, self.db, params)
             places.append(place)
 
         return places
 
-    def create_virus(self):
-        pass
-
-    def create_people(self):
-        people = []
-        for p in self.config["People"].replace({np.nan: None}).to_dict("records"):
-            params = Parameters(p)
-            # params = PersonParameters(p["name"], p["department"])
-            # TODO: review department string DONE
-            person = Person(self.env, self.db, params)
-            person.start()
-            people.append(person)
-            # break
-
-        return people
-
     def create_actions(self):
         return Actions(self.env, self.db)
 
-    def create_options(self):
-        p = self.config["Options"].replace({np.nan: None}).set_index("option")["value"].to_dict()
-        params = Parameters(p)
-        options = Options(self.env, self.db, params)
+    def create_people(self):
+        people = []
+        for p in self.config["people"]:
+            params = Parameters(p)
+            person = Person(self.env, self.db, params)
+            person.start()
+            people.append(person)
 
-        return options
+        return people
+
+
