2f1759078271681e754d199526207f2bbc99819d
imartinezl <inigomlap@gmail.com>
2021-04-14 18:49:25 +0200

    Example json

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ for p in config["places"]:
 
 # FROM JSON
 import json
-with open("example.json", "r") as f:
+with open("data/example.json", "r") as f:
     config = json.load(f)
 
 # preprocess config
