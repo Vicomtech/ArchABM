d1d0d32794c49445eada4b5a5d16d53414765751
imartinezl <inigomlap@gmail.com>
2021-04-14 14:12:57 +0200

    Engine: progress bar and options created first

--- archABM/Engine.py
+++ archABM/Engine.py
@@ -1,5 +1,6 @@
 import simpy
 import logging
+from tqdm import tqdm
 
 from .Creator import Creator
 from .Database import Database
@@ -18,15 +19,18 @@ class Engine:
         self.db.next()
 
         god = Creator(self.env, self.config, self.db)
+        self.db.options = god.create_options()
         self.db.events = god.create_events()
         self.db.places = god.create_places()
-        self.db.options = god.create_options()
         self.db.actions = god.create_actions()
         self.db.people = god.create_people()
 
     def run(self, until, num=1):
         # print("Simulation Started")
-        for i in range(num):
-            self.setup()
-            self.env.run(until)
+        with tqdm(total=num) as pbar:
+            for i in range(num):
+                self.setup()
+                self.env.run(until)
+                pbar.update(1)
         # print("Simulation Finished")
+        return self.db.results.done()
