9de914a6b48d335251fe79da460283c6b0bbce61
imartinezl <inigomlap@gmail.com>
2021-04-22 13:59:18 +0200

    Event: activity => model

--- archABM/Event.py
+++ archABM/Event.py
@@ -1,5 +1,5 @@
 class Event:
-    def __init__(self, activity, place, duration):
-        self.activity = activity
+    def __init__(self, model, place, duration):
+        self.model = model
         self.place = place
         self.duration = duration
