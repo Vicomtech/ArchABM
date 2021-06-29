f94a57b7028a2700f9db7a4b0d489c9e89a12f90
imartinezl <inigomlap@gmail.com>
2021-06-29 10:45:36 +0200

    AerosolModel

--- archABM/AerosolModel.py
+++ archABM/AerosolModel.py
@@ -6,23 +6,6 @@ class AerosolModel:
     def __init__(self, params):
         self.params = params
 
-
-    def get_person_risk(self, params):
-        """Calculate the transmission risk of an individual in a room"
-
-        Args:
-            params (Parameters): dictionary of model parameters
-        """
-        pass
-
-    def get_place_risk(self, params):
-        """Calculate the risk in air quality of a place
-
-        Args:
-            params (Parameters): dictionary of model parameters
-        """
-        pass
-
     def get_risk(self, inputs):
         """Calculate the transmission risk of an individual in a room 
         and the dosis thrown into the air.
--- archABM/AerosolModelMaxPlanck.py
+++ archABM/AerosolModelMaxPlanck.py
@@ -9,24 +9,6 @@ class AerosolModelMaxPlanck(AerosolModel):
     def __init__(self, params):
         self.params = params
 
-
-    def get_person_risk(self, inputs):
-        """Calculate the transmission risk of an individual in a room
-
-        Args:
-            inputs (Parameters): dictionary of model inputs
-        """
-        pass
-        
-
-    def get_place_risk(self, inputs):
-        """Calculate the risk in air quality of a place
-
-        Args:
-            inputs (Parameters): dictionary of model inputs
-        """
-        pass
-
     def get_risk(self, inputs):
         """Calculate the transmission risk of an individual in a room 
         and the dosis thrown into the air.
