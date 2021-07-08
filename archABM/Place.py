2a29c0eb97e0266c45621efe009dd5313dd31f7a
imartinezl <inigomlap@gmail.com>
2021-07-08 11:05:17 +0200

    Place: rm legacy, rm times tracking, frame => snapshot, staticmethod

--- archABM/Place.py
+++ archABM/Place.py
@@ -1,10 +1,11 @@
-import random
+from random import randrange
 
 from simpy import Environment
-from .PropagationModel import PropagationModel
+
 from .Database import Database
 from .Parameters import Parameters
-from .PlaceFrame import PlaceFrame
+from .SnapshotPlace import SnapshotPlace
+
 
 class Place:
     id = -1
@@ -16,12 +17,11 @@ class Place:
         self.env = env
         self.db = db
         self.params = params
-        self.convert_params()
+        # self.convert_params()
 
         self.people = []
         self.num_people = 0
         self.infective_people = 0
-        self.times = {}
 
         self.CO2_baseline = self.db.model.params.CO2_background
         self.CO2_level = self.CO2_baseline
@@ -30,38 +30,15 @@ class Place:
         self.last_updated = 0
 
         self.event = self.get_event()
-        # self.init_propagation_model()
-        # self.aerosol_model = self.db.model
-        self.place_frame = PlaceFrame()
+        self.snapshot = SnapshotPlace()
 
     @classmethod
     def reset(cls) -> None:
         Place.id = -1
 
-    def next(self) -> None:
+    @staticmethod
+    def next() -> None:
         Place.id += 1
-
-    # def init_propagation_model(self) -> None:
-    #     if self.event.params.shared:
-
-    #         self.propagation_model = PropagationModel()
-
-    #         total_mask_efficiency = self.event.params.mask_efficiency
-    #         room_ventilation_rate = self.params.ventilation
-    #         room_area = self.params.area
-    #         room_height = self.params.height
-    #         self.propagation_model.start(total_mask_efficiency, room_ventilation_rate, room_area, room_height)
-
-    def convert_params(self) -> None:
-        # if self.params.department is not None:
-        #     if "," in self.params.department:
-        #         self.params.department = self.params.department.split(",")
-        #     else:
-        #         self.params.department = [self.params.department]
-        if self.params.capacity is not None:
-            self.params.capacity = int(self.params.capacity)
-        else:
-            self.params.capacity = 1000 # TODO: setup maximum capacity DONE
 
     def get_event(self) -> None:
         for e in self.db.events:
@@ -77,30 +54,18 @@ class Place:
         self.people.append(person)
         self.num_people += 1
         self.infective_people += person.status
-        # print("PLACE ADD", self.id, self.num_people)
-
-        # register time
-        if person not in self.times:
-            self.times[person.id] = {}
-        self.times[person.id]["t1"] = self.env.now
 
         # save frame
         self.save_place_frame()
 
     def remove_person(self, person) -> None:
-        # register time
-        self.times[person.id]["t2"] = self.env.now
-        elapsed = self.times[person.id]["t2"] - self.times[person.id]["t1"]
-
         # update air quality
         self.update_air()
-        self.times.pop(person.id, None)
 
         # remove from list
         self.people.remove(person)
         self.num_people -= 1
         self.infective_people -= person.status
-        # print("PLACE DEL", self.id, self.num_people)
 
         # save frame
         self.save_place_frame()
@@ -108,24 +73,26 @@ class Place:
     def update_air(self) -> None:
         elapsed = self.env.now - self.last_updated
         if self.event.params.shared and elapsed > 0:
-            inputs = Parameters({
-                "room_area": self.params.area,
-                "room_height": self.params.height,
-                "room_ventilation_rate": self.params.ventilation,
-                "mask_efficiency": self.event.params.mask_efficiency,
-                "event_duration": elapsed / 60,
-                "num_people": self.num_people,
-                "infective_people": self.infective_people,
-                "CO2_level": self.CO2_level
-            })
+            inputs = Parameters(
+                {
+                    "room_area": self.params.area,
+                    "room_height": self.params.height,
+                    "room_ventilation_rate": self.params.ventilation,
+                    "mask_efficiency": self.event.params.mask_efficiency,
+                    "event_duration": elapsed / 60,
+                    "num_people": self.num_people,
+                    "infective_people": self.infective_people,
+                    "CO2_level": self.CO2_level,
+                }
+            )
             CO2_level, infection_risk = self.db.model.get_risk(inputs)
 
-            # UPDATE PLACE
+            # update place
             self.CO2_level = CO2_level
             self.elapsed += elapsed
             self.infection_risk += elapsed * (infection_risk - self.infection_risk) / self.elapsed
 
-            # UPDATE PEOPLE
+            # update people # TODO: review if we need to update the risk of infected people as well
             for p in self.people:
                 p.update(elapsed, infection_risk, CO2_level)
         self.last_updated = self.env.now
@@ -133,18 +100,16 @@ class Place:
     def people_attending(self) -> int:
         if self.full():
             return 0
-        # print("ATTENDING", self.params.name, self.params.capacity, self.num_people)
-        return random.randrange(int(self.params.capacity - self.num_people))
+        return randrange(int(self.params.capacity - self.num_people))
 
     def full(self) -> bool:
         return self.params.capacity == self.num_people
 
     def save_place_frame(self) -> None:
-        # self.place_frame.reset()
-        self.place_frame.set("run", self.db.run)
-        self.place_frame.set("time", self.env.now, 0)
-        self.place_frame.set("place", self.id)
-        self.place_frame.set("num_people", self.num_people)
-        self.place_frame.set("CO2_level", self.CO2_level, 2)
-        self.place_frame.set("infection_risk", self.infection_risk, 4)
-        self.db.results.write_place(self.place_frame)
+        self.snapshot.set("run", self.db.run)
+        self.snapshot.set("time", self.env.now, 0)
+        self.snapshot.set("place", self.id)
+        self.snapshot.set("num_people", self.num_people)
+        self.snapshot.set("CO2_level", self.CO2_level, 2)
+        self.snapshot.set("infection_risk", self.infection_risk, 4)
+        self.db.results.write_place(self.snapshot)
