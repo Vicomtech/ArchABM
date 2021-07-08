6b3af2c33f193d2425bb2bca54fe2568d4a8602f
imartinezl <inigomlap@gmail.com>
2021-07-08 11:04:16 +0200

    Removed commented and legacy code, formatting

--- archABM/EventGenerator.py
+++ archABM/EventGenerator.py
@@ -1,5 +1,7 @@
 import random
+
 from simpy import Environment
+
 from .Database import Database
 
 
@@ -21,65 +23,36 @@ class EventGenerator:
 
     def generate(self, now, person):
         # Get probabilities for each model event
-        num_people = len(self.db.people)
         p = [m.probability(now) for m in self.models]
         v = [m.valid() for m in self.models]
 
-        # print(round(now), p, v)
-
         # Select event model
         if sum(p) > 0:
-            # p = p / np.sum(p)
             model = random.choices(self.models, weights=p)[0]
-            # model = np.random.choice(self.models, size=None, replace=False, p=p)
         elif sum(v) > 0:
-            # TODO: what happens if all probabilities are zero? => random event? DONE
-            # v = v / np.sum(v)
             model = random.choices(self.models, weights=v)[0]
-            # model = np.random.choice(self.models, size=None, replace=False, p=v)
-            # print(now, p, v, model)
         else:
             model = random.choice(self.models)
-            # model = np.random.choice(self.models, size=None, replace=False)
-        # model.consume() # TODO: consume once if not collective, consume for every person if collective
 
         # Create event based on selected model
         activity = model.params.activity
-        # activity = model
         duration = model.duration(now)
         # duration += 0.001
         place = self.db.actions.find_place(model, person)
         if place is None:
             return None
         if model.params.collective:
-            # print("COLLECTIVE", place.params.name)
-            return self.db.actions.create_collective_event(
-                model, place, duration, person
-            )
+            return self.db.actions.create_collective_event(model, place, duration, person)
         else:
             model.consume()
             return self.db.actions.create_event(model, place, duration)
 
-    # def consume_activity(self, model):
-    #     model.consume()
     def consume_activity(self, model):
         for m in self.models:
             if m.params.activity == model.params.activity:
                 m.consume()
 
-    # def consume_activity(self, activity):
-    #     for m in self.models:
-    #         if m.params.activity == activity:
-    #             m.consume()
-
-    # def valid_activity(self, model):
-    #     model.consume()
     def valid_activity(self, model):
         for m in self.models:
             if m.params.activity == model.params.activity:
                 return m.valid()
-
-    # def valid_activity(self, activity):
-    #     for m in self.models:
-    #         if m.params.activity == activity:
-    #             return m.valid()
