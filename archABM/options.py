1c3a2b8034eeb7a64369ced30e39917571422d56
imartinezl <inigomlap@gmail.com>
2021-07-11 10:31:45 +0200

    Docs: options

--- archABM/options.py
+++ archABM/options.py
@@ -5,6 +5,12 @@ from .parameters import Parameters
 
 
 class Options:
+    """Saves general options for the simulation
+    """
+    env: Environment
+    db: Database
+    params: Parameters
+
     def __init__(self, env: Environment, db: Database, params: Parameters) -> None:
         self.env = env
         self.db = db
