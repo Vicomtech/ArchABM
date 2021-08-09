6ee7f60572a02860fc4489f9fe629ded555a0103
imartinezl <inigomlap@gmail.com>
2021-08-09 11:47:16 +0200

    Experiments run script

--- run.py
+++ run.py
@@ -2,11 +2,10 @@ import json
 from archABM.engine import Engine
 
 experiments = range(8)
-# experiments = [0,1,3]
+
 for xp in experiments:
-    with open("data/config_" + str(xp) + ".json", "r") as f:
+    with open("experiments/config_" + str(xp) + ".json", "r") as f:
         print(xp)
         config = json.load(f)
     simulation = Engine(config)
     results = simulation.run()
-# print(results)
