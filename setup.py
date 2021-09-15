a3516de102373f466fd8a8b218dac47cd586bf4c
imartinezl <inigomlap@gmail.com>
2021-09-15 17:36:18 +0200

    Updated version minor number

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ TEST_SUITE = "tests"
 REQUIREMENTS = ["simpy", "tqdm", "jsonschema", "typer[all]"]
 SETUP_REQUIREMENTS = []
 TEST_REQUIREMENTS = ["pytest", "pytest-cov"]
-VERSION = "0.2.3"
+VERSION = "0.2.4"
 
 setuptools.setup(
     author=AUTHOR,
