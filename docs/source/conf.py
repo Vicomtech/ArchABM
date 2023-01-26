a57c4f229181ab707a6666c1041fe6c88cd4ec64
imartinezl <inigomlap@gmail.com>
2023-01-26 16:38:19 +0100

    Changelog

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = "2021, Vicomtech"
 author = "Vicomtech"
 
 # The full version, including alpha/beta/rc tags
-release = "0.3.0"
+release = "0.4.0"
 
 
 # -- General configuration ---------------------------------------------------
